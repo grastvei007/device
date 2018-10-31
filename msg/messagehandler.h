bc432c35b680cd669bdbc5ccb13873b778303161
Stein Gunnar Grastveit <grastvei@pvv.org>
2018-10-31 16:51:15 +0100

    add sceleton to automatic translation of messages from a serial device

---
+++ msg/messagehandler.h
@@ -0,0 +1,12 @@
+#ifndef MESSAGEHANDLER_H
+#define MESSAGEHANDLER_H
+
+#include <QObject>
+
+class MessageHandler
+{
+public:
+    MessageHandler();
+};
+
+#endif // MESSAGEHANDLER_H
---
+++ msg/messagehandlermanager.h
@@ -0,0 +1,12 @@
+#ifndef MESSAGEHANDLERMANAGER_H
+#define MESSAGEHANDLERMANAGER_H
+
+#include <QObject>
+
+class MessageHandlerManager
+{
+public:
+    MessageHandlerManager();
+};
+
+#endif // MESSAGEHANDLERMANAGER_H
